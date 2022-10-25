e34e19a3ee254d97d60f0db35a80bb46f43251a8
Roy <roymarzan.21@ichat.sp.edu.sg>
2022-10-25 11:39:02 +0800

    Exercise 3

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,29 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduciton to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduciton to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+def display_main_menu():
+    print("Enter some number seperated by commas (e.g. 5, 67, 32, ...)")
+
+def get_user_input():
+    user_input = input()
+    num_list = list()
+    input_list = user_input.split(",")
+    for items in input_list:
+        num_list.append(float(items.strip()))
+    #print(input_list)
+    #print (num_list)
+    return (num_list)
+#def get_user_input():
+#    list = input().split(",")
+#    for i in range(0, len(list)):
+#        list[i] = float(list[i])
+#        print(i)
+#    print(list)
+
+def calc_average():
+    print("calc_average")
+
+if (__name__ == "__main__"):
+    main()
--- bmi.py
+++ bmi.py
@@ -1,13 +1,13 @@
-def calculate_bmi (height, weight) :
+def calculate_bmi (height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
     bmi = weight / (height * height)
     print("BMI = " + str(bmi))
-    if (bmi < 18.5) :
+    if (bmi < 18.5):
         print("You are Under Weight")
-    elif (bmi >= 18.5 and bmi <= 25.0) :
+    elif (bmi >= 18.5 and bmi <= 25.0):
         print("You are Normal Weight")
-    else :
+    else:
         print("You are Over Weight")
 
 calculate_bmi(weight=58, height=1.70)
